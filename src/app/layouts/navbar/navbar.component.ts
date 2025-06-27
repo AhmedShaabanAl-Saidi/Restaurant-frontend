e805d8d5a5395c02b1c694a6f53013b4fac35ec9
AhmedShaabanAl-Saidi <ahmedshaaban123321@gmail.com>
2025-06-28 02:57:45 +0300

    Enhance navbar functionality and styles; add routing to links and improve layout

--- src/app/layouts/navbar/navbar.component.ts
+++ src/app/layouts/navbar/navbar.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from '../../pages/home/home.component';
+import { AboutComponent } from '../../pages/about/about.component';
+import { MenuComponent } from '../../pages/menu/menu.component';
+import { ContactComponent } from '../../pages/contact/contact.component';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { BlogComponent } from '../../pages/blog/blog.component';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
-export class NavbarComponent {
-
-}
+export class NavbarComponent {}
