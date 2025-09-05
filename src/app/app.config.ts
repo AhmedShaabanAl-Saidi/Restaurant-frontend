ec109fb9c045e90775cb0443fa7ee0a3c45b7aac
AhmedShaabanAl-Saidi <ahmedshaaban123321@gmail.com>
2025-09-05 20:30:42 +0300

    Apply errors Interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,16 +5,21 @@ import {
   provideClientHydration,
   withEventReplay,
 } from '@angular/platform-browser';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withFetch,
+  withInterceptors,
+} from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideToastr } from 'ngx-toastr';
+import { errorsInterceptor } from './core/interceptors/errors/errors.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
-    provideHttpClient(withFetch()),
+    provideHttpClient(withFetch(), withInterceptors([errorsInterceptor])),
     provideAnimations(),
     provideToastr(),
     provideToastr({
--- src/app/core/interceptors/errors/errors.interceptor.ts
+++ src/app/core/interceptors/errors/errors.interceptor.ts
@@ -1,5 +1,15 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { catchError, throwError } from 'rxjs';
 
 export const errorsInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
+  const toastrService = inject(ToastrService);
+
+  return next(req).pipe(
+    catchError((err) => {
+      toastrService.error(err.error.message, 'Bistro Team');
+      return throwError(() => err);
+    })
+  );
 };
