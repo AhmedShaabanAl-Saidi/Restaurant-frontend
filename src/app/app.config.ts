f3f5cb6358d754a30e0c9feef4fe7df403da7e83
AhmedShaabanAl-Saidi <ahmedshaaban123321@gmail.com>
2025-09-05 20:36:48 +0300

    Apply ViewTransitions & scrollPositionRestoration to Top

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,9 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {
+  provideRouter,
+  withInMemoryScrolling,
+  withViewTransitions,
+} from '@angular/router';
 import { routes } from './app.routes';
 import {
   provideClientHydration,
@@ -17,7 +21,11 @@ import { errorsInterceptor } from './core/interceptors/errors/errors.interceptor
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(
+      routes,
+      withViewTransitions(),
+      withInMemoryScrolling({ scrollPositionRestoration: 'top' })
+    ),
     provideClientHydration(withEventReplay()),
     provideHttpClient(withFetch(), withInterceptors([errorsInterceptor])),
     provideAnimations(),
